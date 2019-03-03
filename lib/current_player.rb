a552dc7bab6612deab76fc469fd80adc1c314091
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-03 21:55:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,11 +2,11 @@ def turn_count(turn)
 
   move_count = []
   turn.each do |unit|
-  
+
     if unit == "X" || unit == "O"
-      
+
       move_count.push(1)
-    
+
     end
   end
   move_count.length.to_i
